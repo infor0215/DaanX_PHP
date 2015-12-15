f2ec4078d1abfdb6f0be97c4e75c11ec0b899e43
Lian0123 <lienyungli123@gmail.com>
2015-12-15 14:27:43 +0800

    2015/12/15更新
    1.舊的Bug已抵掉
    2.分類系統完成
    3.增加註解
    4.未轉Json

--- Daan-X.php
+++ Daan-X.php
@@ -8,8 +8,10 @@ Daan-X的主程式
 	mysql_query("SET NAMES utf8");//資料庫編碼設定
 	include"phpQuery-onefile.php";//引入phpQuery的code
 	include"get_web_last.php";//引入資料表"紀錄的陣列變數"
-		for ($A=0; $A <count($array_unmber) ; $A++) {//當有新資料時 計算陣列數並執行迴圈
-			include"get_web_main.php";//引入"寫入資料庫的頁面內容"
+		if ($read_data==1) {
+			for ($A=0; $A <count($array_unmber) ; $A++) {//當有新資料時 計算陣列數並執行迴圈
+				include"get_web_main.php";//引入"寫入資料庫的頁面內容"
+			}
 		}
 	mysql_close($mysql_sign);//登出SQL
 ?>
--- get_web_main.php
+++ get_web_main.php
@@ -2,14 +2,15 @@
 /*=========================
 本程式已完成
 include的程式
+第一階段的程式
 ==========================*/
-	$ch=curl_init();//curl宣告
-	curl_setopt($ch, CURLOPT_URL,"http://ta.taivs.tp.edu.tw/news/news.asp?KEY=$array_unmber[$A]");//網頁來源宣告
-	curl_setopt($ch, CURLOPT_HEADER, false);//頁面標籤顯示
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//顯示頭信息？
-	$web_data=curl_exec($ch);//取網頁原始碼
-	$doc=phpQuery::newDocumentHTML($web_data);//將抓來的資料丟到phpQuery的code
-	$do_pq=pq('table[width="100%"]&&[align="center"]',$doc);
+	$ch=curl_init();//curl宣告(第一階段)
+	curl_setopt($ch, CURLOPT_URL,"http://ta.taivs.tp.edu.tw/news/news.asp?KEY=$array_unmber[$A]");//網頁來源宣告(第一階段)
+	curl_setopt($ch, CURLOPT_HEADER, false);//頁面標籤顯示(第一階段)
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//顯示頭信息？(第一階段)
+	$web_data=curl_exec($ch);//取網頁原始碼(第一階段)
+	$doc=phpQuery::newDocumentHTML($web_data);//將抓來的資料丟到phpQuery的code(第一階段)
+	$do_pq=pq('table[width="100%"]&&[align="center"]',$doc);//取得內容頁面並將不需要的程式碼刪除(第一階段)
 	$web_main_top_day=$do_pq->find('td[align="center"]&&[width="13%"]&&[bgcolor="#FF0000"]')->text();//日期
 	$web_main_top_title=$do_pq->find('td[align="left"]&&[width="87%"]&&[bgcolor="#FF0000"]')->text();//主題
 	$web_main_data=iconv("big5","UTF-8",$do_pq->find('tr[bgcolor="#FFDDBB"]')->find('td[width="87%"]')->html());//內文
@@ -41,9 +42,43 @@ include的程式
 	$Address_long=$Address_end-$Address_frist;//抓字首和字尾的差
 	$web_main_file_check_output=substr($web_main_file_from,$Address_end,3);//抓末三字
 	$web_main_file_output=substr($web_main_file_from,$Address_frist+10,$Address_long-7);//抓超連結文字
+		$web_main_file="";//初始變數
 		if($web_main_file_check_output =="jpg"){//當末三字是jpg時（我還沒看過學校資料上有png或gif的文章附件）
 			$web_main_file="http://ta.taivs.tp.edu.tw/news/$web_main_file_output";//圖片輸出
 		}
-	mysql_query("INSERT INTO stu_main (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
-	curl_close($ch);//結束php_curl
+/*=========================
+本程式已完成
+include的程式
+第二階段的程式
+==========================*/
+	$http = array('http://ta.taivs.tp.edu.tw/news/news.asp?SearchRole=%BC%D0%C3D&SearchWord=&SearchWay=%BE%C7%A5%CD%A8%C6%B0%C8&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchRole=%BC%D0%C3D&SearchWord=&SearchWay=%AC%A1%B0%CA%A7%D6%B3%F8&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchWay=%C4v%C1%C9&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchWay=%BAa%C5A%BA%5D&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchWay=%BC%FA%BE%C7%AA%F7%A4%BD%A7i&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchRole=%BC%D0%C3D&SearchWord=&SearchWay=%B5%F9%A5U%B8%C9%A7U%B4%EE%A7K&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchWay=no2&board=1','http://ta.taivs.tp.edu.tw/news/news.asp?SearchRole=%BC%D0%C3D&SearchWord=&SearchWay=%B7s%BE%C7%B4%C1%AD%AB%ADn%A4%BD%A7i&board=1');//抓取的網頁陣列表(第二階段)
+		for ($n=0; $n <count($http) ; $n++) { //抓取網頁取網頁的陣列迴圈
+		$ch_stu_text=curl_init();//curl宣告(第二階段)
+		curl_setopt($ch_stu_text, CURLOPT_URL,"$http[$n]");//網頁來源宣告(第二階段)
+		curl_setopt($ch_stu_text, CURLOPT_HEADER, false);//頁面標籤顯示(第二階段)
+		curl_setopt($ch_stu_text, CURLOPT_RETURNTRANSFER, true);//顯示頭信息？(第二階段)
+		$web_stu_text_data=curl_exec($ch_stu_text);//取網頁原始碼(第二階段)
+		$stu_text_doc=phpQuery::newDocumentHTML($web_stu_text_data);//將抓來的資料丟到phpQuery的code(第二階段)
+		$cut_stu_text=explode("\n", pq('table[border="1"]',$stu_text_doc)->find('tr[bgcolor]')->find('a')->text());//取得標題的文字
+			for ($o=0; $o<count($cut_stu_text) ; $o++) { //搜尋該頁面資料數以執行迴圈
+					if ($n<2&&substr($web_main_top_title,4,40)==substr($cut_stu_text[$o],0,40)) {//當資料內容相同且對應輸出資料庫的網頁陣列數＄n＝0,1
+						mysql_query("INSERT INTO stu_main_stu_affairs (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
+					}
+					else if ($n<4&&substr($web_main_top_title,4,40)==substr($cut_stu_text[$o],0,40)) {//當資料內容相同且對應輸出資料庫的網頁陣列數＄n＝2,3
+						mysql_query("INSERT INTO stu_main_stu_race (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
+					}
+					else if($n<6&&substr($web_main_top_title,4,40)==substr($cut_stu_text[$o],0,40)) {//當資料內容相同且對應輸出資料庫的網頁陣列數＄n＝4,5
+						mysql_query("INSERT INTO stu_main_stu_help (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
+					}
+					else if ($n==6&&substr($web_main_top_title,4,40)==substr($cut_stu_text[$o],0,40)) {//當資料內容相同且對應輸出資料庫的網頁陣列數＄n＝6
+						mysql_query("INSERT INTO stu_main_this_week (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
+					}
+					else if ($n==7&&substr($web_main_top_title,4,40)==substr($cut_stu_text[$o],0,40)) {//當資料內容相同且對應輸出資料庫的網頁陣列數＄n＝7
+						mysql_query("INSERT INTO stu_main_this_term (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫
+					}
+			}
+	curl_close($ch_stu_text);//結束php_curl
+	}
+	mysql_query("INSERT INTO stu_main (web_id,web_main_top_day,web_main_top_title,web_main_data,web_main_where,web_main_outside_link,web_main_can_read_time,web_main_link,web_main_file) VALUES ('$array_unmber[$A]','$web_main_top_day','$web_main_top_title','$web_main_data','$web_main_where','$web_main_outside_link','$web_main_can_read_time','$web_main_link','$web_main_file') ") ;//寫入資料庫（總數據）
+	curl_close($ch);//結束php_curl(第一階段)
 ?>
